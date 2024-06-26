4c2b0752db2ccb96c2678333ac7e626b90024cfb
Marek Freunscht <freunscht58@outlook.de>
2024-06-26 03:41:57 +0200

    Added login and register forms

--- app/app.py
+++ app/app.py
@@ -6,6 +6,14 @@ from .server.rendering import get_fragment_shader
 
 app = Flask(__name__,)
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 @app.route("/fragment")
 def fragment_shader():
     args = {}
